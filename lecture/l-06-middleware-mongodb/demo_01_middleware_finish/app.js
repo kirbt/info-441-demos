306711931ad1d9afa977ab55f7c3ce7064d89143
Kyle <kmthayer@uw.edu>
2025-10-14 11:10:47 -0700

    finish middleware demo

--- lecture/l-06-middleware-mongodb/demo_01_middleware_finish/app.js
+++ lecture/l-06-middleware-mongodb/demo_01_middleware_finish/app.js
@@ -19,6 +19,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    console.log("This is the first additional piece of middleware that I made")
+    next()
+    console.log("The first middleware gets one last chance to do something")
+})
+
+app.use((req, res, next) => {
+    console.log("Second middleware adds information to the request to be used later")
+    req.testValue = 3
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log(
+        "Third middleware, looks up the testValue, which is",
+        req.testValue
+    )
+    next()
+})
+
 app.use('/users', usersRouter);
 
 export default app;
