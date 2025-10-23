eae687528e5697cbe46d96f3defce1a8f9eb5a78
Kyle <kmthayer@uw.edu>
2025-10-23 11:41:10 -0700

    finish session demo

--- lecture/l-09-sessions-auth/demo-2-session-end/app.js
+++ lecture/l-09-sessions-auth/demo-2-session-end/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import sessions from 'express-session'
 
 import usersRouter from './routes/users.js';
 
@@ -18,6 +19,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const oneDay = 1000 * 60 * 60 * 24
+app.use(sessions({
+    secret: "This is some secret key I am making up 05n5yf5398hoiueneue",
+    saveUninitialized: true,
+    cookie: {maxAge: oneDay},
+    resave: false
+}))
 
 app.use('/users', usersRouter);
 
