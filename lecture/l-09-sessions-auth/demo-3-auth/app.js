8673a7b3cb75852a06f4d2b1a004bdf16df32331
Kyle <kmthayer@uw.edu>
2025-10-23 12:08:54 -0700

    finish the auth demo (hopefully)

--- lecture/l-09-sessions-auth/demo-3-auth/app.js
+++ lecture/l-09-sessions-auth/demo-3-auth/app.js
@@ -4,6 +4,31 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import sessions from 'express-session'
 
+// 	npm install https://gitpkg.now.sh/kylethayer/ms-identity-javascript-nodejs-tutorial-msal-node-v2-/Common/msal-node-wrapper?main
+// (for mac install, you might need to put quotes around the package name)
+
+import WebAppAuthProvider from 'msal-node-wrapper'
+
+const authConfig = {
+    auth: {
+    	clientId: "Client ID or Application ID HERE",
+    	authority: "https://login.microsoftonline.com/Paste_the_Tenant_directory_ID_Here",
+    	clientSecret: "Client or Application secret here (NOT THE 'secret id', but the 'secret value')",
+    	redirectUri: "/redirect"
+    },
+	system: {
+    	loggerOptions: {
+        	loggerCallback(loglevel, message, containsPii) {
+            	console.log(message);
+        	},
+        	piiLoggingEnabled: false,
+        	logLevel: 3,
+    	}
+	}
+};
+
+
+
 import usersRouter from './routes/users.js';
 
 import { fileURLToPath } from 'url';
@@ -13,6 +38,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 var app = express();
+
+app.enable('trust proxy')
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,6 +55,26 @@ app.use(sessions({
     resave: false
 }))
 
+const authProvider = await WebAppAuthProvider.WebAppAuthProvider.initialize(authConfig);
+app.use(authProvider.authenticate());
+
+
 app.use('/users', usersRouter);
+
+
+app.get('/signin', (req, res, next) => {
+   	 return req.authContext.login({
+   		 postLoginRedirectUri: "/", // redirect here after login
+   	 })(req, res, next);
+    
+});
+app.get('/signout', (req, res, next) => {
+   	 return req.authContext.logout({
+   		 postLogoutRedirectUri: "/", // redirect here after logout
+   	 })(req, res, next);
+    
+});
+app.use(authProvider.interactionErrorHandler());
+
 
 export default app;
--- lecture/l-09-sessions-auth/demo-3-auth/routes/users.js
+++ lecture/l-09-sessions-auth/demo-3-auth/routes/users.js
@@ -3,34 +3,13 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  if(req.session.userid){
-    res.send(`Here is the information for you (${req.session.userid})`)
+  if(req.session.isAuthenticated){
+    res.send(`Here is the information for you:
+      your account name is: ${req.session.account.name}
+      and your username is: ${req.session.account.username}`)
   } else {
     res.send("error: You must be logged in")
   }
 });
 
-router.post('/login', (req, res) => {
-
-  if(req.session.userid){
-    res.send("Error: you are already logged in as " + req.session.userid )
-    return
-  }
-
-  // check username and password
-  if(req.body.username == "kylethayer" && req.body.password == "asdasd"){
-    req.session.userid = "kylethayer"
-    res.send("you logged in")
-  } else if(req.body.username == "anotheruser" && req.body.password == "pwd"){
-    req.session.userid = "anotheruser"
-    res.send("you logged in")
-  } else {
-    res.send("wrong login info")
-  }
-})
-
-router.post('/logout', (req, res) => {
-  req.session.destroy()
-  res.send("you are logged out")
-})
 export default router;
